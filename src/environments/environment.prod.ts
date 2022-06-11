57766bb294da054fed3caeb10fe0a5b1a2f843cd
mbabaei <mbabaei0@gmail.com>
2022-06-12 00:43:30 +0430

    fix: add env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://covid-193.p.rapidapi.com',
+  apiHost:'covid-193.p.rapidapi.com',
+  apiKey:'4e226df344msh01761e2d47fca9cp1cd556jsncd61b0edbe91',
 };
